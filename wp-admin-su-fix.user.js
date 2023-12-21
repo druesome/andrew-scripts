372d60e5acc17f615cf3f8a0fa6e0db9a2ece06f
Andrew dela Serna <drew@automattic.com>
2023-12-22 01:19:41 +0800

    Update wp-admin-su-fix.user.js
    Includes links in the Posts and Pages sections

--- wp-admin-su-fix.user.js
+++ wp-admin-su-fix.user.js
@@ -2,8 +2,8 @@
 // @name         WP-Admin SU Fix
 // @namespace    wp-admin-su-fix
 // @namespace    http://wordpress.com/
-// @version      0.1
-// @description  Redirect to clicked wp-admin link after SSO login.
+// @version      0.2
+// @description  Redirect to the WordPress admin panel link after SSO login
 // @updateURL	 https://github.com/druesome/andrew-scripts/raw/main/wp-admin-su-fix.user.js
 // @downloadURL	 https://github.com/druesome/andrew-scripts/raw/main/wp-admin-su-fix.user.js
 // @author       druesome
@@ -30,16 +30,28 @@
                 e.preventDefault();
                 var targetUrl = this.getAttribute('href');
                 console.log('Admin link clicked. Target URL:', targetUrl);
-                // Set the URL in Tampermonkey's storage
                 GM_setValue('postSSORedirect', targetUrl);
-                // Log the value to confirm it's been set
                 console.log('Tampermonkey Storage - Saved Redirect URL:', GM_getValue('postSSORedirect'));
-
-                // Redirect to the clicked URL
                 window.location.href = targetUrl;
             });
         });
     }
+
+function handleAdditionalLinks() {
+    var additionalLinks = document.querySelectorAll('a[href*="/post/"], a[href*="/page/"]');
+    console.log('Found ' + additionalLinks.length + ' additional links.');
+
+    additionalLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            var targetUrl = "https://wordpress.com" + this.getAttribute('href');
+            console.log('Additional link clicked. Target URL:', targetUrl);
+            GM_setValue('postSSORedirect', targetUrl);
+            console.log('Tampermonkey Storage - Saved Redirect URL:', GM_getValue('postSSORedirect'));
+            window.location.href = targetUrl;
+        });
+    });
+}
 
     function onDomContentLoaded() {
         console.log('DOM fully loaded.');
@@ -50,22 +62,21 @@
             console.log('Saved Redirect URL:', postSSORedirect);
             if (postSSORedirect) {
                 console.log('Redirecting to saved URL.');
-                GM_setValue('postSSORedirect', null); // Clear the stored URL
+                GM_setValue('postSSORedirect', null);
                 window.location.href = postSSORedirect;
             }
         } else {
             handleAdminLinks();
-            // Set up a MutationObserver to watch for changes in the DOM
+            handleAdditionalLinks();
             const observer = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutation) {
                     if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-                        // Re-run the handleAdminLinks function to catch any new admin links
                         handleAdminLinks();
+                        handleAdditionalLinks();
                     }
                 });
             });
 
-            // Start observing the body for added nodes
             observer.observe(document.body, {
                 childList: true,
                 subtree: true
