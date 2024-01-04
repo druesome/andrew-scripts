0e2c6f8a4ad380e2ebccac87c4b3ecf0b9156a31
Andrew dela Serna <drew@automattic.com>
2024-01-04 22:34:16 +0800

    Update insta-copy-site-url.user.js
    Added tweaks to allow secondary domains to be copied as well.

--- insta-copy-site-url.user.js
+++ insta-copy-site-url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Insta-Copy Site URL
 // @namespace    insta-copy-site-url
-// @version      1.2
+// @version      1.3
 // @description  Instantly copies URL of user's sites in one click.
 // @updateURL	   https://github.com/druesome/andrew-scripts/raw/main/insta-copy-site-url.user.js
 // @downloadURL	 https://github.com/druesome/andrew-scripts/raw/main/insta-copy-site-url.user.js
@@ -15,7 +15,7 @@
 
 
 GM_addStyle ( `
-  span.cp_btn {
+  span.site-domain {
   transition: all 0.3s;
   border-radius: 3px;
 
@@ -25,7 +25,7 @@ GM_addStyle ( `
   }
 }
 
-span.cp_btn {
+span.site-domain {
     font-style: normal;
     font-size: 13px;
     color: #222;
@@ -39,15 +39,39 @@ var $ = window.jQuery;
 function constructLinks() {
     $('.user__info_container .sites .site:not(.thisdone)').each(function() {
         var $site = $(this);
-        var siteURL = $site.find('.site-domain').first().text();
         $site.addClass('thisdone');
-        var $siteDomain = $site.find('.site-domain').first();
-        $siteDomain.addClass('cp_btn');
-        $siteDomain.on('click', function() {
-            copy_url(siteURL, $siteDomain);
+
+        // Find all instances of site-domain within the site
+        var $siteDomains = $site.find('.site-domain');
+
+        // Process each instance of site-domain
+        $siteDomains.each(function(index) {
+            var $siteDomain = $(this);
+            var siteURL = $siteDomain.text();
+
+                // If the site-domain is the first one, add the cpy_btn class and handle click event
+                $siteDomain.addClass('cpy_btn');
+                $siteDomain.on('click', function() {
+                    copy_url(siteURL, $siteDomain);
+                });
+
+            // Move the .darc span beside the URL and add the "DARC" text inside it
+            var $darcSpan = $siteDomain.next('.darc');
+            if ($darcSpan.length) {
+                $siteDomain.after($darcSpan);
+            }
+
+            // Remove the surrounding link
+            $siteDomain.closest('a').contents().unwrap();
+
+            // Build a new link around the DARC span and make it open in a new tab
+            var $darcLink = $('<a>').attr('href', 'https://mc.a8c.com/tools/reportcard/domain/?domain=' + encodeURIComponent(siteURL)).attr('target', '_blank').append($darcSpan);
+            $siteDomain.after($darcLink);
         });
     });
 }
+
+
 
 // Copy the URL
 
