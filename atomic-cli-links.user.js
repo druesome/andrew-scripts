e1715697ad3a2ed53d30911275f0fd28d678d2be
Andrew dela Serna <drew@automattic.com>
2024-05-08 21:02:24 +0800

    Update atomic-cli-links.user.js
    Added links to VPMC and Revert tool

--- atomic-cli-links.user.js
+++ atomic-cli-links.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Atomic CLI Links
 // @namespace    atomic-cli-links
-// @version      1.1
-// @description  Adds links to CLI on Atomic sites in Zendesk.
+// @version      1.2
+// @description  Adds links to CLI, VPMC and Revert tools on Atomic sites in Zendesk.
 // @updateURL	 https://github.com/druesome/andrew-scripts/raw/main/atomic-cli-links.user.js
 // @downloadURL	 https://github.com/druesome/andrew-scripts/raw/main/atomic-cli-links.user.js
 // @author       druesome
@@ -21,8 +21,25 @@ function addcliLinks() {
         if ($atomicSpan.length > 0 && !$site.hasClass('cliadded')) {
             $site.addClass('cliadded');
             var siteURL = 'https://' + $site.find('.site-domain').first().text();
+
+            // Extract blog ID from Videos link
+            var videosLink = $site.find('.site-links a[title="VideoPress files"]');
+            var blogId = videosLink.attr('href').match(/blog=(\d+)/)[1];
+
+            // Add link to Revert tool using extracted blog ID
+            var revertLink = $('<a>').attr('href', 'https://mc.a8c.com/automated-transfer/revert.php?blog_id=' + blogId).attr('target', '_blank').text('Revert');
+            var revertListItem = $('<li>').append(revertLink);
+            $site.find('.site-links').append(revertListItem);
+
+            // Add link to VPMC
+            var vpmcLink = $('<a>').attr('href', 'https://mc.vaultpress.com/site/blog_id=' + blogId).attr('target', '_blank').text('VPMC');
+            var vpmcListItem = $('<li>').append(vpmcLink);
+            $site.find('.site-links').append(vpmcListItem);
+
+            // Add link to CLI
             var cliLink = $('<a>').attr('href', siteURL + '/_cli').attr('target', '_blank').text('CLI');
-            $site.find('.site-links').append($('<li>').append(cliLink));
+            var cliListItem = $('<li>').append(cliLink);
+            $site.find('.site-links').append(cliListItem);
         }
     });
 }
